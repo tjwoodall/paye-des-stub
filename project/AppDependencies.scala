89addbb617f7e7e24d468ecd7bd55c70ec878099
ryandac <ryan.da.costa@digital.hmrc.gov.uk>
2022-05-31 17:13:17 +0100

    DDCE-3087 - Upgrade paye des stub to latest play framework

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,20 +5,20 @@ object AppDependencies {
 
   private val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "bootstrap-backend-play-28"    % "5.7.0",
-    "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-28"           % "0.51.0",
-    "uk.gov.hmrc"        %% "domain"                       % "6.1.0-play-28",
-    "uk.gov.hmrc"        %% "tax-year"                     % "1.4.0",
+    "uk.gov.hmrc"        %% "bootstrap-backend-play-28"    % "5.24.0",
+    "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-28"           % "0.64.0",
+    "uk.gov.hmrc"        %% "domain"                       % "8.1.0-play-28",
+    "uk.gov.hmrc"        %% "tax-year"                     % "1.7.0",
     "uk.gov.hmrc"        %% "hmrc-stubs-core"              % "6.2.0-play-26"
   )
 
   private val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % "0.51.0",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % "0.64.0",
     "org.scalatest"          %% "scalatest"                % "3.2.9",
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0",
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.9.0",
     "com.vladsch.flexmark"   % "flexmark-all"              % "0.35.10",
-    "org.mockito"            % "mockito-core"              % "2.10.0",
+    "org.mockito"            % "mockito-core"              % "4.6.0",
     "org.pegdown"            % "pegdown"                   % "1.6.0",
     "com.typesafe.play"      %% "play-test"                % PlayVersion.current,
     "org.scalaj"             %% "scalaj-http"              % "2.4.2"
