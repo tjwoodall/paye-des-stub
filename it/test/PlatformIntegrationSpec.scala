ac2d89f9b510fc79a815c31b97c0ee4a8ec524d1
SachaMcCormack <175815565+SachaMcCormack@users.noreply.github.com>
2024-10-09 14:07:20 +0100

    Revert "DDCE-6083 - Remove scopes from API definition"

--- it/test/PlatformIntegrationSpec.scala
+++ it/test/PlatformIntegrationSpec.scala
@@ -43,6 +43,7 @@ class PlatformIntegrationSpec extends AnyWordSpecLike with Matchers with GuiceOn
       val apiDefinitionJson: JsValue = Json.parse(
         s"""
           |{
+          |    "scopes": [],
           |    "api": {
           |        "name": "Individual PAYE Test Support",
           |        "description": "$description",
